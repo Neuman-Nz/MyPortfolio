d1c30cfec63add3c125d1f1bbd75323dd3d4643f
neuman <owiroadams@gmail.com>
2024-07-26 19:06:54 +0300

    working on the home section

---
+++ src/components/features/Features.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <section 
+         id='features'
+         className='w-full h-auto pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black'>
+            Features
+        </section>
+    )
+}
+
+export default Features
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {logo} from "../../assets/index"
+import { Link } from "react-scroll";
+import {logo} from "../../assets/index";
 import { navlinksdata } from '../../constants'; 
 
 const Navbar = () => {
     return(
-        <div className="w-full h-20 mx-auto flex justify-between items-center">
+        <div className="w-full h-20 mx-auto flex justify-between items-center font-titleFont">
             <div className='w-20 h-10 '>
                 <img src={logo} alt="logo" />
             </div>
@@ -15,7 +16,16 @@ const Navbar = () => {
                             <li
                              className='text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300'
                              key={_id}> 
+                             <Link 
+                              activeClass='active'
+                              to={link}
+                              spy={true}
+                              smooth={true}
+                              offset={-70}
+                              duration={500}
+                              >
                              {title}
+                             </Link>
                             </li>
                         ))
                     }
