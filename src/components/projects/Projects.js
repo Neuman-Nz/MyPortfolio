9db47cd812d72a0fcd134d51b93a84ab81ec7c0f
neuman <owiroadams@gmail.com>
2024-07-27 01:06:05 +0300

    Started working on the projects section

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Title from "../layouts/Title";
+
+const Projects = () => {
+  return (
+    <section 
+      id='projects'
+      className='w-full py-20 border-b-[1px] border-b-bleck'
+    >
+    <div className='flex justify-center items-center'>
+    <Title
+          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          des="My Projects"
+        />
+    </div>
+    </section>
+  )
+}
+
+export default Projects
