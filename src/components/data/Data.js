8a28e1893ab60023bd299c9b8117bcee3054077a
neuman <owiroadams@gmail.com>
2024-07-29 14:23:26 +0300

    added my socials on the icons

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -41,29 +41,65 @@ export const featuresData = [
   },
 ];
 
+// export const projectData = [
+//   {
+//     id: 1,
+//     des: " This is a project that I was part of as a scram master. The project dwelled on full stack development as React and Flask was used as the main Frameworks",
+//     src: "https://i.postimg.cc/QdNRZmBS/project1.png",
+//     title: "Job-seeker Application"
+//   },
+//   {
+//     id: 2,
+//     des: " Currently working on this web application as the customization is closely being monitized by the client. It involves active use of React(frontend) and later on Flask for its backend",
+//     src: "https://i.postimg.cc/1zXXXDdr/project2.png",
+//     title: "PBO Website"
+//   },
+//   {
+//     id: 3,
+//     des: " As a freelancer, I developed a customized based website for an individual business owner using HTML,CSS,Bootstrap,and Javascript",
+//     src: "https://i.postimg.cc/6q0YHshK/project3.png",
+//     title: "Customized Business Website"
+//   },
+//   {
+//     id: 4,
+//     des: " A mini-application that I was part of that which is used to track sugar levels of the users as it guides on nutrient intake",
+//     src: "https://i.postimg.cc/tTd5T0w6/project4.png",
+//     title: "Suger-Tracker Application"
+//   }
+// ]
+
+
 export const projectData = [
   {
     id: 1,
-    des: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    des: " This is a project that I was part of as a scram master. The project dwelled on full stack development as React and Flask was used as the main Frameworks",
     src: "https://i.postimg.cc/QdNRZmBS/project1.png",
-    title: "Job-seeker Application"
+    title: "Job-seeker Application",
+    github: "https://github.com/Neuman-Nz/Job-seeker-App",
+    live: "https://careergo.onrender.com/"
   },
   {
     id: 2,
-    des: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    des: " Currently working on this web application as the customization is closely being monitized by the client. It involves active use of React(frontend) and later on Flask for its backend",
     src: "https://i.postimg.cc/1zXXXDdr/project2.png",
-    title: "PBO Website"
+    title: "PBO Website",
+    github: "https://github.com/Neuman-Nz/MINDAfrica_Application",
+    live: "https://mind-africa-web.vercel.app/"
   },
   {
     id: 3,
-    des: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    des: " As a freelancer, I developed a customized based website for an individual business owner using HTML,CSS,Bootstrap,and Javascript",
     src: "https://i.postimg.cc/6q0YHshK/project3.png",
-    title: "E-commerce Website"
+    title: "Customized Business Website",
+    github: "https://github.com/Neuman-Nz/WAKHANA-LIMITED",
+    live: "https://wakhana-limited.vercel.app/"
   },
   {
     id: 4,
-    des: " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!",
+    des: " A mini-application that I was part of that which is used to track sugar levels of the users as it guides on nutrient intake",
     src: "https://i.postimg.cc/tTd5T0w6/project4.png",
-    title: "Suger-Tracker Application"
+    title: "Sugar-Tracker Application",
+    github: "https://github.com/Neuman-Nz/Sugar-Tracker",
+    live: "https://sugar-tracker-pink.vercel.app/"
   }
 ]
--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,3 +1,39 @@
+// import React from 'react'
+// import { BsGithub } from "react-icons/bs";
+// import { FaGlobe } from "react-icons/fa";
+
+// const ProjectCard = ({data}) => {
+
+//     console.log("data", data)
+
+//   return (
+//     <div className='w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000'>
+//         <div> 
+//             <div className='w-full h-[80% object-cover group-hover:scale-110 duration-300 cursor-pointer]'><img src={data?.src} alt="src"/></div> <br/>
+//             <div className='flex items-center justify-between'>
+//             <div>
+//                 <h1 className='taxt-base uppercase text-designColor font-normal'>{data?.title}</h1>
+//             </div>  
+//             <div>
+//             <div className="flex gap-2">
+//               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+//                 <BsGithub />
+//               </span>
+//               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+//                 <FaGlobe />
+//               </span>
+//             </div>
+//             </div>  
+//             </div> <br/>
+//             <div className='text-sm tracking-wide mt-3 hover:text-gray-100 duration-300'><p>{data?.des}</p></div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default ProjectCard
+
+
 import React from 'react'
 import { BsGithub } from "react-icons/bs";
 import { FaGlobe } from "react-icons/fa";
@@ -12,16 +48,16 @@ const ProjectCard = ({data}) => {
             <div className='w-full h-[80% object-cover group-hover:scale-110 duration-300 cursor-pointer]'><img src={data?.src} alt="src"/></div> <br/>
             <div className='flex items-center justify-between'>
             <div>
-                <h1 className='taxt-base uppercase text-designColor font-normal'>{data?.title}</h1>
+                <h1 className='text-base uppercase text-designColor font-normal'>{data?.title}</h1>
             </div>  
             <div>
             <div className="flex gap-2">
-              <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+              <a href={data?.github} target="_blank" rel="noopener noreferrer" className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
                 <BsGithub />
-              </span>
-              <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+              </a>
+              <a href={data?.live} target="_blank" rel="noopener noreferrer" className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
                 <FaGlobe />
-              </span>
+              </a>
             </div>
             </div>  
             </div> <br/>
