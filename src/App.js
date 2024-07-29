48b654817608089e8e3caff492b586690b3914a0
neuman <owiroadams@gmail.com>
2024-07-29 13:18:39 +0300

    made the site responsive

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import FooterBottom from "./components/footer/FooterBottom";
 
 function App() {
   return (
-    <div className="w-full h-auto bg-bodyColor text-lightText">
-      <div className="max-w-screen-xl mx-auto px-16">
+    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+      <div className="max-w-screen-xl mx-auto">
           <Navbar />
           <Banner/>
           <Features/>
